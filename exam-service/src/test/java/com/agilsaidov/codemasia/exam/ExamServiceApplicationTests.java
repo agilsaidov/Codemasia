66776631d7285e1abe9148d84141bd97e8be8c25
Agil Saidov <agilsaidov01@gmail.com>
2026-05-15 23:05:04 +0400

    init: project structure and infrastructure setup

---
+++ exam-service/src/test/java/com/agilsaidov/codemasia/exam/ExamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agilsaidov.codemasia.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ submission-service/src/main/java/com/agilsaidov/codemasia/submission/SubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agilsaidov.codemasia.submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmissionServiceApplication.class, args);
+	}
+
+}
